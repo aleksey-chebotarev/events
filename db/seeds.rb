abd4ad48aad8a5e483d2a1fc77ef83ec07867e9f
Alexey Chebotarev <alexey.chebotarev21@gmail.com>
2017-12-07 23:55:24 +0300

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,32 @@
 require 'ffaker'
 
 regions = [
-  { number: 04, name: 'Алтай' },
+  { number: 4, name: 'Алтай' },
   { number: 61, name: 'Ростов' },
   { number: 62, name: 'Рязань' },
   { number: 91, name: 'Крым' }
+]
+
+cities = [
+  { 4 => { name: 'Алейск' } },
+  { 4 => { name: 'Барнаул' } },
+  { 4 => { name: 'Белокуриха' } },
+  { 4 => { name: 'Бийск' } },
+
+  { 61 => { name: 'Ростов-на-Дону' } },
+  { 61 => { name: 'Гуково' } },
+  { 61 => { name: 'Зерноград' } },
+  { 61 => { name: 'Новочеркасск' } },
+
+  { 62 => { name: 'Рязань' } },
+  { 62 => { name: 'Сасово' } },
+  { 62 => { name: 'Скопин' } },
+  { 62 => { name: 'Спас-Клепики' } },
+
+  { 91 => { name: 'Симферополь' } },
+  { 91 => { name: 'Судак' } },
+  { 91 => { name: 'Феодосия' } },
+  { 91 => { name: 'Ялта' } }
 ]
 
 regions.each do |n|
@@ -14,4 +36,13 @@ regions.each do |n|
   )
 
   puts "Region #{region.name} was created"
+
+  cities.each do |city|
+    city = City.create!(
+      name: city[region.number][:name],
+      region_id: region.id
+    )
+
+    puts "City #{city.name} was created"
+  end
 end
